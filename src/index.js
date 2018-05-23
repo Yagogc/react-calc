5895bdc31e4a9557dca00d61019bd19eddd72b03
Yago Gonzalez <jagogc@gmail.com>
2018-05-23 01:30:31 +0100

    Added styles to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { injectGlobal } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+
+injectGlobal`
+	* {
+		margin: 0;
+		padding: 0;
+		font-family: Orbitron, sans-serif;
+		box-sizing: border-box;
+  }
+  html {
+    background: #f4f7fb;
+  }
+`;
