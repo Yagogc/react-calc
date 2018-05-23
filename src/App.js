c8abd2de08c2b782049cd98df57fab7dc0f29eb3
Yago Gonzalez <jagogc@gmail.com>
2018-05-23 01:44:54 +0100

    App updated and test.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <Container>Hello World!</Container>;
   }
 }
 
 export default App;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
+Container.displayName = "div";
